35858dd6fa23c93b78fddba3ad39b7ab6fe465f0
Polina Korkina <pvkorkina@gmail.com>
2024-01-23 20:16:46 +0500

    Добавляет начальную версию проекта

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      /* Указываем символ "@" как псевдоним для пути к директории "./src" */
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
