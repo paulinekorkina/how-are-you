543a59ced3f43e5b30d630583f5de22976d705cd
Polina Korkina <pvkorkina@gmail.com>
2024-05-07 19:45:10 +0500

    Добавляет подгрузку состояний (WIP)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
     <router-link
       :to="{ name: 'condition', params: { id: condition.id }}"
       class="col-12 md:col-6 lg:col-3 flex"
-      v-for="condition in conditionsStore.conditionsExtended"
+      v-for="condition in conditionsToRender"
       :key="condition.id"
     >
       <condition-card
@@ -17,13 +17,35 @@
       v-if="conditionsStore.conditions.length === 0"
       class="col-12 mt-5 text-lg text-center"
     >Список состояний пуст. Добавьте первое!</div>
+
   </div>
+  <button v-if="isMoreConditions" type="button" @click="loadMoreConditions">load more</button>
 </template>
 
 <script setup>
+import { ref, computed, watch } from 'vue';
+import { storeToRefs } from 'pinia';
 import useConditionsStore from '@/stores/conditions';
 import ConditionCard from '@/modules/conditions/ConditionCard.vue';
 import FiltersBlock from '@/modules/filters/FiltersBlock.vue';
 
 const conditionsStore = useConditionsStore();
+
+const conditionsCountPerStep = 8;
+const renderedConditionsCount = ref(conditionsCountPerStep);
+
+const { conditionsExtended: conditions } = storeToRefs(conditionsStore);
+
+const conditionsToRender = computed(() => conditions.value
+  .slice(0, Math.min(conditions.value.length, renderedConditionsCount.value)));
+
+const isMoreConditions = computed(() => conditions.value.length > renderedConditionsCount.value);
+
+const loadMoreConditions = () => {
+  renderedConditionsCount.value += conditionsCountPerStep;
+};
+
+watch(conditions, () => {
+  renderedConditionsCount.value = conditionsCountPerStep;
+});
 </script>
